45deaaa52016fffce6b8c748ac38999207b173d6
Matthew Gibbons <mhgibbons@me.com>
2024-11-09 08:02:11 +0000

    Add unique symbol to StringField interface for type safety

--- src/types/string-field.ts
+++ src/types/string-field.ts
@@ -3,6 +3,7 @@ interface StringField {
 	readonly pattern?: string;
 	readonly enum?: readonly string[];
 	readonly default?: string;
+	readonly __stringField: unique symbol;
 }
 
 export type { StringField };
