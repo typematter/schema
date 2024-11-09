4b0f051bfb3f225fd711ca8d7c5b65c0ebd5cb67
Matthew Gibbons <mhgibbons@me.com>
2024-11-09 07:58:44 +0000

    Add validationError import and export to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import schemaField from '$lib/schema-field/index.js';
+import validationError from '$lib/validation-error/index.js';
 import type { SchemaField } from '$types/schema-field.js';
 import type { ValidationError } from '$types/validation-error.js';
 
-export { schemaField as default, type SchemaField, type ValidationError };
+export { schemaField as default, validationError, type SchemaField, type ValidationError };
